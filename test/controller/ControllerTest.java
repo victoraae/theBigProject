7c53c042aaf1e9be9b51ec6b96b602e21d221201
victor <au670070@uni.au.dk>
2023-11-28 08:30:50 +0100

    iteration2, elaboration, test wip

---
+++ test/controller/ControllerTest.java
@@ -0,0 +1,40 @@
+package controller;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void opretLager() {
+    }
+
+    @Test
+    void opretReol() {
+    }
+
+    @Test
+    void opretHylde() {
+    }
+
+    @Test
+    void opdaterLager() {
+    }
+
+    @Test
+    void sletLager() {
+    }
+
+    @Test
+    void sletReol() {
+    }
+
+    @Test
+    void sletHylde() {
+    }
+}
