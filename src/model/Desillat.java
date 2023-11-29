fc613706162be2a4713a8dbd4601c4c465e873c3
mathias1000zzz <mathqrr@gmail.com>
2023-11-29 10:45:27 +0100

    Destillat Klassen

---
+++ src/model/Desillat.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Desillat {
+    private LocalDate dato;
+    private double alkoholProcent;
+    private String ansvarlig;
+    private double liter;
+    private int antalGange;
+    private String rygemateriale;
+    //retning 0..* --> 1 korn
+    private Korn korn;
+    //retining 1 --> 0..* newmake
+    private List<NewMake> newMakes = new ArrayList<>();
+
+    public Desillat(LocalDate dato, double alkoholProcent, String ansvarlig, double liter, int antalGange, String rygemateriale, Korn korn) {
+        this.dato = dato;
+        this.alkoholProcent = alkoholProcent;
+        this.ansvarlig = ansvarlig;
+        this.liter = liter;
+        this.antalGange = antalGange;
+        this.rygemateriale = rygemateriale;
+        this.korn = korn;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public double getAlkoholProcent() {
+        return alkoholProcent;
+    }
+
+    public String getAnsvarlig() {
+        return ansvarlig;
+    }
+
+    public double getLiter() {
+        return liter;
+    }
+
+    public int getAntalGange() {
+        return antalGange;
+    }
+
+    public String getRygemateriale() {
+        return rygemateriale;
+    }
+
+    public void setAntalGange(int antalGange) {
+        this.antalGange = antalGange;
+    }
+}
