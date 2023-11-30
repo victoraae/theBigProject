e3bdf6541b8e86747fb3b4539f22e1fe77a037d2
mathias1000zzz <mathqrr@gmail.com>
2023-11-30 09:30:42 +0100

    Tilføjet ny gui

--- src/gui/HovedVindue.java
+++ src/gui/HovedVindue.java
@@ -4,17 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.NoSuchElementException;
 
-public class Gui extends Application {
+public class HovedVindue extends Application {
 
+    private Stage parent;
     @Override
     public void start(Stage stage) throws Exception {
-        URL fxmlFileName = this.getClass().getResource("Untitled.fxml");
+        URL fxmlFileName = this.getClass().getResource("hovedOversigt.fxml");
         if (fxmlFileName == null) throw new NoSuchElementException("FXML file not found");
 
         Parent root = FXMLLoader.load(fxmlFileName);
@@ -22,6 +25,19 @@ public class Gui extends Application {
         stage.setMinHeight(root.minHeight(-1));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        parent = stage;
         stage.show();
     }
+
+    private void openSubWindow(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sub_fxml_file.fxml"));
+        Stage root = loader.load();
+        Scene scene = new Scene(root);
+        root.initOwner(parent);
+        root.setScene();
+        root.setTitle("Subvindue");
+
+        root.show();
+    }
 }
+
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,13 +1,12 @@
 package gui;
 
 import controller.Controller;
-import controller.Storage;
 import javafx.application.Application;
 import storage.ListStorage;
 
 public class Main {
     public static void main(String[] args) {
        Controller.setStorage(new ListStorage());
-        Application.launch(Gui.class);
+        Application.launch(HovedVindue.class);
     }
 }
