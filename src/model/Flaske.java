8af8a19e592c8f6e8c7a4064ba888dd160becfee
victoraae <73301911+victoraae@users.noreply.github.com>
2023-12-06 13:05:39 +0100

    opretWhisky i controlleren er lavet, og en toString for flaske er påbegyndt

--- src/model/Flaske.java
+++ src/model/Flaske.java
@@ -1,8 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Flaske {
     private int nummer;
-    public Flaske(int nummer) {
+    private final Whisky whisky;
+    private int antalFlasker;
+    public Flaske(int nummer, Whisky whisky, int antalFlasker) {
         this.nummer = nummer;
+        this.whisky = whisky;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        int år = whisky.getÅr();
+        String kval = whisky.getKvalitetsstempel();
+
+        sb.append(whisky.getNavn() + ", flaske nr: " + nummer + "ud af " + antalFlasker + "\n");
+        sb.append("Denne " + kval + "  whisky har lagret i " + år +  " år ");
+
+        return sb.toString();
     }
 }
